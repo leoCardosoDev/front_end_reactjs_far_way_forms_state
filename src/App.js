ade64e5acaf72afdd1419fd029bbc4d0d87fd938
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-28 15:42:01 -0700

    feat: add derivate status

--- src/App.js
+++ src/App.js
@@ -108,15 +108,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Comece adicionando um item na sua cesta!</em>
+      </p>
+    )
+  }
   // Derivate
   const qtdItems = items.length
   const numPacked = items.filter((item) => item.packed).length
   const percent = Math.round((numPacked / qtdItems) * 100)
-  const result = `👜 Você tem ${qtdItems} itens em sua lista, e na sua mala já tem
-  ${numPacked} itens (${percent}%)`
+
   return (
     <footer className="stats">
-      <em>{percent === 100 ? 'Você está pronto para viajar! ✈️' : result}</em>
+      <em>
+        {percent === 100
+          ? 'Você está pronto para viajar! ✈️'
+          : `👜 Você tem ${qtdItems} itens em sua lista, e na sua mala já tem
+  ${numPacked} itens (${percent}%)`}
+      </em>
     </footer>
   )
 }
